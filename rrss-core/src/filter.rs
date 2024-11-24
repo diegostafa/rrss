699ac9d8d8edd1f0ab77099bf15fe4a21f424c1e
diegostafa <dstafa.dev@gmail.com>
2024-11-24 23:28:15 +0100

    combine filter tests

--- rrss-core/src/filter.rs
+++ rrss-core/src/filter.rs
@@ -70,56 +70,59 @@ impl Filter {
 }
 impl FilterTest<Feed> for Filter {
     fn test(&self, e: &Feed) -> bool {
+        let mut test = true;
         if let Some(tag) = &self.tag_id {
-            return e.conf.tags.contains(tag);
+            test = test && e.conf.tags.contains(tag);
         }
         if let Some(id) = &self.feed_id {
-            return e.id() == id;
+            test = test && e.id() == id;
         }
         if let Some(_) = &self.unread_feed {
-            return e.tot_unread() > 0;
+            test = test && e.tot_unread() > 0;
         }
         if let Some(p) = &self.feed_contains {
-            return e.name().to_ascii_lowercase().contains(&p.to_lowercase());
+            test = test && e.name().to_ascii_lowercase().contains(&p.to_lowercase());
         }
-        true
+        test
     }
 }
 impl FilterTest<Item> for Filter {
     fn test(&self, e: &Item) -> bool {
+        let mut test = true;
         if let Some(id) = &self.item_id {
-            return e.data.id == *id;
+            test = test && e.data.id == *id;
         }
         if let Some(true) = self.unread_item {
-            return !e.state.is_read;
+            test = test && !e.state.is_read;
         }
         if let Some(false) = self.unread_item {
-            return e.state.is_read;
+            test = test && e.state.is_read;
         }
         if let Some(p) = &self.item_contains {
             if let Some(title) = &e.data.title {
-                return title.to_lowercase().contains(&p.to_lowercase());
+                test = test && title.to_lowercase().contains(&p.to_lowercase());
             }
             if let Some(content) = &e.data.content {
-                return content.to_lowercase().contains(&p.to_lowercase());
+                test = test && content.to_lowercase().contains(&p.to_lowercase());
             }
-            return false;
+            test = test && false;
         }
         if let Some(_) = self.unfiltered {
-            return !e.state.is_filtered;
+            test = test && !e.state.is_filtered;
         }
-        true
+        test
     }
 }
 impl FilterTest<Tag> for Filter {
     fn test(&self, e: &Tag) -> bool {
+        let mut test = true;
         if let Some(id) = &self.tag_id {
-            return e.name == *id;
+            test = test && e.name == *id;
         }
         if let Some(p) = &self.tag_contains {
-            return e.name.to_ascii_lowercase().contains(&p.to_lowercase());
+            test = test && e.name.to_ascii_lowercase().contains(&p.to_lowercase());
         }
-        true
+        test
     }
 }
 impl std::fmt::Display for Filter {
